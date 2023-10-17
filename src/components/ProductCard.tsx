333a4fbc2ae61d0eefe505b2ada30a4031412021
julioemc2 <julioemc2@gmail.com>
2023-10-17 13:38:38 -0400

    paso 3

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,57 @@
+import React, { createContext } from 'react';
+
+import { useProduct } from '../hooks/useProduct';
+import { InitialValues, Product, ProductCardHandlers, ProductContextProps, onChangeArgs } from '../interfaces/interfaces';
+
+import styles from '../styles/styles.module.css';
+
+
+export const ProductContext = createContext({} as ProductContextProps);
+const { Provider } = ProductContext;
+
+
+export interface Props {
+    product: Product;
+    //children?: ReactElement | ReactElement[];
+    children: ( args: ProductCardHandlers ) => JSX.Element;
+    className?: string;
+    style?: React.CSSProperties;
+    onChange?: ( args: onChangeArgs ) => void;
+    value?: number;
+    initialValues?: InitialValues;
+}
+
+
+export const ProductCard = ({ children, product, className, style, onChange, value, initialValues }: Props) => {
+
+
+    // TODO: custom hook
+    const { counter, increaseBy, maxCount, isMaxCountReached, reset } 
+        = useProduct({ onChange, product, value, initialValues });
+
+    return (
+        <Provider value={{
+            counter,
+            increaseBy,
+            maxCount,
+            product
+        }}>
+            <div 
+                className={ `${ styles.productCard } ${ className }` }
+                style={ style }
+            >
+                { 
+                    children({
+                        count: counter,
+                        isMaxCountReached: !!initialValues?.count && initialValues.maxCount === counter,
+                        maxCount: initialValues?.maxCount,
+                        product,
+
+                        increaseBy,
+                        reset,
+                    }) 
+                }
+            </div>
+        </Provider>
+    )
+}
